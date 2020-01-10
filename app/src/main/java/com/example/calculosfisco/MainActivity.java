8a59e3ce74bbf929cd59c38ace2a55235e0aed32
Eduardo <lemmoncollection@gmail.com>
2020-01-10 15:45:00 -0300

    Implementado RBT12 activity com linear layout

--- app/src/main/java/com/example/calculosfisco/MainActivity.java
+++ app/src/main/java/com/example/calculosfisco/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.calculosfisco;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -11,16 +12,27 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    Button rbtButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        rbtButton = (Button)findViewById(R.id.btrbt12);
+
+        rbtButton.setOnClickListener(new btRBT12Clicked());
+
+
+
+        /*
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,8 +41,21 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+         */
+
+
     }
 
+    class btRBT12Clicked implements Button.OnClickListener{
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(MainActivity.this, "AEW2", Toast.LENGTH_SHORT).show();
+            Intent myIntent = new Intent(MainActivity.this, RbtActivity.class);
+            MainActivity.this.startActivity(myIntent);
+
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -52,4 +77,6 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
 }
---
+++ app/src/main/java/com/example/calculosfisco/RbtActivity.java
@@ -0,0 +1,14 @@
+package com.example.calculosfisco;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RbtActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rbt);
+    }
+}
